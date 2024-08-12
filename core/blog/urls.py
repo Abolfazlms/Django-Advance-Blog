3262fe1c4dea7243b180e2b7430cd85b09b4e0b6
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-08-12 13:14:39 +0330

    some class base views are added

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,11 +1,18 @@
 
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import indexView
+from django.views.generic import RedirectView
+# from .views import indexView
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('fbv-index/',indexView,name='fbv-index'),
+    # path('fbv-index/',indexView,name='fbv-index'),
     # path('cbv-index/', TemplateView.as_view(template_name="index.html",extra_context={"name":"ali"})),
-    path('cbv-index/',views.IndexView.as_view(),name='cbv-index')
+    path('cbv-index/',views.IndexView.as_view(),name='cbv-index'),
+    # path('go-to-maktabkhooneh',RedirectView.as_view(url='https://www.maktabkhooneh.com'),name='go-to-maktabkhooneh')
+    # path('go-to-index',RedirectView.as_view(pattern_name='blog:cbv-index'),name='go-to-index')
+    path('go-to-maktabkhooneh/<int:pk>',views.RedirectToMaktabkhooneh.as_view(),name='go-to-maktabkhooneh'),
+    path('post/',views.PostList.as_view(),name='post-list')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,17 +1,44 @@
 from typing import Any
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import get_object_or_404, render
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
 from .models import Post
 # Create your views here.
+
+#Function Base View show a template
+'''
 def indexView(request):
+    """
+    a function based view to show index page
+    """
     name = "ali"
     context={"name":name}
     return render(request,"index.html",context)
-
+'''
 class IndexView(TemplateView):
+    """
+    a class based view to show index page
+    """
     template_name = 'index.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context ['name'] = 'ali'
         context ['post'] = Post.objects.all()
         return context
+
+''' FBV To Redirect
+from django.shortcuts import redirect
+def RedirectToMaktabkhooneh(request):
+    return redirect('https://www.maktabkhooneh.com')
+'''
+class RedirectToMaktabkhooneh(RedirectView):
+    url='https://maktabkhooneh.com'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args,**kwargs)
+    
+class PostList(ListView):
+    model = Post
+    context_object_name = 'posts'
