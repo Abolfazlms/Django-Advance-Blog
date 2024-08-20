8a66e4186550a0587e2bbc06973c71b433d3a432
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-08-20 18:50:23 +0330

    model view set are added to project.

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Post, Category
 
 # class PostSerlizer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
 
-class PostSerlizer(serializers.ModelSerializer):
-
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         # fields = '__all__'
         fields = ['id','title','content','author','category','created_date','published_date','status']
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        # fields = '__all__'
+        fields = ['id','name']
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -10,8 +10,8 @@ from rest_framework import mixins
 from rest_framework import viewsets
 
 
-from .serializer import PostSerlizer
-from blog.models import Post
+from .serializer import PostSerializer, CategorySerializer
+from blog.models import Post, Category
 
 from django.shortcuts import get_object_or_404
 
@@ -23,11 +23,11 @@ from rest_framework.decorators import api_view, permission_classes
 def postList(request):
     if request.method == 'GET':
         post = Post.objects.filter(status=True)
-        serializer = PostSerlizer(post,many=True)
+        serializer = PostSerializer(post,many=True)
         return Response(serializer.data)
     
     elif request.method == 'POST':
-        serializer = PostSerlizer(data=request.data)
+        serializer = PostSerializer(data=request.data)
         # if serializer.is_valid():
         #     serializer.save()
         #     return Response(serializer.data)
@@ -43,7 +43,7 @@ def postList(request):
 def postDetail(request,id):
     # try:
     #     post = Post.objects.get(pk=id)
-    #     serializer = PostSerlizer(post)
+    #     serializer = PostSerializer(post)
     #     return Response(serializer.data)  
     # except Post.DoesNotExist:
         # return Response({'detail':'post does not exist.'},status=404)
@@ -51,10 +51,10 @@ def postDetail(request,id):
         #    
     post = get_object_or_404(Post,pk=id,status=True)    
     if request.method == 'GET':       
-        serializer = PostSerlizer(post)
+        serializer = PostSerializer(post)
         return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = PostSerlizer(post,data=request.data)
+        serializer = PostSerializer(post,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
@@ -70,16 +70,16 @@ from rest_framework.views import APIView
 class PostList(APIView):
     '''getting a list of posts and creating new posts'''
     permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerlizer
+    serializer_class = PostSerializer
     def get(self,request):
         '''retrieving a list of posts'''
         post = Post.objects.filter(status=True)
-        serializer = PostSerlizer(post,many=True)
+        serializer = PostSerializer(post,many=True)
         return Response(serializer.data)
     
     def post(self,request):
         '''creating a post with provided data'''
-        serializer = PostSerlizer(data=request.data)
+        serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
@@ -87,18 +87,18 @@ class PostList(APIView):
 class PostDetail(APIView):
     '''getting detail of the post and edit plus remove it.'''
     permission_classes = [IsAuthenticated]
-    serializer_class = PostSerlizer
+    serializer_class = PostSerializer
 
     def get(self, request, id):        
         '''retrieving the post data'''
         post = get_object_or_404(Post,pk=id,status=True)  
-        serializer = PostSerlizer(post)
+        serializer = PostSerializer(post)
         return Response(serializer.data)  
     
     def put(self, request, id):
         '''editing the post data'''
         post = get_object_or_404(Post,pk=id,status=True) 
-        serializer = PostSerlizer(post,data=request.data)
+        serializer = PostSerializer(post,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
@@ -112,38 +112,24 @@ class PostDetail(APIView):
 class PostList(ListCreateAPIView):
     '''getting a list of posts and creating new posts'''
     permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerlizer
+    serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
     '''getting detail of the post and edit plus remove it.'''
     permission_classes = [IsAuthenticated]
-    serializer_class = PostSerlizer
+    serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     #lookup_field = 'id' #custom name for parameter or change it on urls.py (default : pk)  
 
 # Example foe View set in class base view
-class PostViewSet(viewsets.ViewSet):
+class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerlizer
+    serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
 
-    def list(self, request):
-        serializer = self.serializer_class(self.queryset,many=True)
-        return Response(serializer.data)
-    def retrieve(self, request, pk=None):
-        queryset = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.serializer_class(queryset)
-        return Response(serializer.data)
-    def create(self,request):
-        pass
-    def update(self, request, pk=None):
-        pass
+class CategoryModelViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
 
-    def partial_update(self, request, pk=None):
-        pass
-
-    def destroy(self, request, pk=None):
-        post = get_object_or_404(Post,pk=pk,status=True)
-        post.delete()
-        return Response({'detail':'item remove successfully.'},status=status.HTTP_204_NO_CONTENT)
