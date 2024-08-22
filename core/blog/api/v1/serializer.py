663334564c0fbe63236ae9ba9abe44a67803ff2c
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-08-22 09:43:42 +0330

    to_representation method for represent data are added .

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -5,7 +5,11 @@ from blog.models import Post, Category
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
-
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        # fields = '__all__'
+        fields = ['id','name']
 class PostSerializer(serializers.ModelSerializer):
 
     # content = serializers.ReadOnlyField()
@@ -13,16 +17,33 @@ class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
     absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+    # category = serializers.SlugRelatedField(
+    #     many=False,
+    #     slug_field='name',
+    #     queryset=Category.objects.all()
+    #     )
+    # category = CategorySerializer()
     class Meta:
         model = Post
         # fields = '__all__'
-        fields = ['id','title','content','author','category','snippet','status','relative_url','absolute_url','created_date','published_date']
+        fields = ['id','title','image','content','author','category','snippet','status','relative_url','absolute_url','created_date','published_date']
         # read_only_fields = ['content']
     def get_abs_url(self, obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        # fields = '__all__'
-        fields = ['id','name']
+    def to_representation(self,instance):
+        request = self.context.get('request')
+        # print(request.__dict__)
+        rep = super().to_representation(instance)
+        rep ['state'] = 'list'
+        if request.parser_context.get('kwargs').get('pk'):
+            # rep ['state'] = 'single'
+            rep.pop('snippet', None)
+            rep.pop('relative_url', None)
+            rep.pop('absolute_url', None)
+        else:
+            rep.pop('content', None)
+        
+        rep['category'] = CategorySerializer(instance.category).data
+        
+        return rep
