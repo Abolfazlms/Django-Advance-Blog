447051de95286793866825f2a1a6e54f85146286
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-08-27 10:51:20 +0330

    django custom authentication system are added.

---
+++ core/accounts/api/v1/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import RegistrationSerializer
+
+
+class RegistrationApiView(generics.GenericAPIView):
+    serializer_class = RegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            data={
+                'email':serializer.validated_data['email']
+            }
+            # return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(data,status=status.HTTP_201_CREATED)
+        # return self.create(request, *args, **kwargs)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
