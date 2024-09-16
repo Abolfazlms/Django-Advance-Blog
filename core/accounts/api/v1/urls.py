06f7caad173cd45b396c58d62a6d32144b58cd23
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-09-16 09:16:07 +0330

    feature: login with Token
    login with token system are added to project.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import include, path
 from . import views
-
+# from rest_framework.authtoken.views import ObtainAuthToken
 app_name = 'api-v1'
 urlpatterns = [
     # registration
@@ -9,5 +9,7 @@ urlpatterns = [
     # change password
     # reset password
     # login token
+    path('token/login/',views.CustomObtainAuthToken.as_view(),name='token-login'),
+    path('token/logout/',views.CustomDiscardAuthToken.as_view(),name='token-logout'),
     # login jwt
 ]
