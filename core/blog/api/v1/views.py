e6845a6ed3a5f9ccb7b92a33b380954c1e865772
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-09-19 17:39:25 +0330

    JWT SYSTEM: jwt system are added

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -133,7 +133,7 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
 """
 # Example foe View set in class base view
 class PostModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
