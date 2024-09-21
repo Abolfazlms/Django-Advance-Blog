6606ae8ccb3ad16d79855dc29a082bca41ddefed
Abolfazlms <Abolfazlms14002021@gmail.com>
2024-09-21 14:39:20 +0330

    Test : model test are completed.

--- core/blog/tests/test_blog_model.py
+++ core/blog/tests/test_blog_model.py
@@ -4,16 +4,18 @@ from datetime import datetime
 from ..models import Post
 from accounts.models import User, Profile
 class TestPostModel(TestCase):
-    def test_create_post_with_valid_data(self):
-        user = User.objects.create_user(email='test@test.com',password='12345678@')
-        profile = Profile.objects.create(
-            user=user,
+    def setUp(self):
+        self.user = User.objects.create_user(email='test@test.com',password='12345678@')
+        self.profile = Profile.objects.create(
+            user=self.user,
             first_name='test_name',
             last_name='test_last_name',
             description='test description about test user',
             )
+
+    def test_create_post_with_valid_data(self):
         post = Post.objects.create(
-            author = profile,
+            author = self.profile,
             title = "test",
             content = "test description",
             status = True,
@@ -21,3 +23,4 @@ class TestPostModel(TestCase):
             published_date = datetime.now(),
         )
         self.assertEquals(post.title,'test')
+        self.assertTrue(Post.objects.filter(pk=post.id).exists())
